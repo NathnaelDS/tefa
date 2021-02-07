5de7823a6613bf4584ca89f5b28c26274707b3c5
NathnaelDS <nathnaelds@gmail.com>
2021-02-07 15:08:54 +0300

    chore: Add new assets

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router-dom";
 import BackArrow from "./icons/backArrow";
 import Info from "./icons/info";
 
-function Header({ name, Icon, color }) {
+function Header({ name, icon, color }) {
   const history = useHistory();
   return (
     <div
@@ -16,9 +16,9 @@ function Header({ name, Icon, color }) {
         <BackArrow />
         <div className="hidden pl-2 text-sm font-bold sm:block">Back</div>
       </div>
-      <div className="flex flex-col items-center">
-        <Icon />
-        <div className="mt-1 text-xs font-bold uppercase">{name}</div>
+      <div className="flex items-center justify-center">
+        <img src={icon} alt="" className="" />
+        <div className="absolute mt-1 text-lg font-bold">{name}</div>
       </div>
       <div className="p-1 bg-white rounded-full shadow-lg cursor-pointer">
         <a href="#basicInfo">
